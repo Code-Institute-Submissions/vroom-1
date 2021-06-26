30175e5b2347e4562ef8d8a9f397e591ce02779c
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-06-26 20:25:58 +0200

    feat: get positions and add them to the table

--- assets/js/displayData.js
+++ assets/js/displayData.js
@@ -10,27 +10,67 @@ async function callAPI(endPoint) {
       let result = await response.json();
       let data = result.response;
       console.log(data);
+      displayDriverStanding(data);
     })
     .catch((err) => {
       console.error(err);
     });
 }
 
+// DRIVER RANKINGS
 let endPointDR = `rankings/drivers?season=2021`;
 callAPI(endPointDR);
 
+// retrieve positions from response and store in array
+function getPosition(data) {
+  let position = [];
+  data.forEach(function (element) {
+    let pos = element.position;
+    position.push(pos);
+  });
+  return position;
+}
+
+// add Driverpositions to table
+function addPositions(data) {
+  const tableDriverStandings = document.getElementById('driverStandings');
+  const bodyDriverStandings = document.getElementById('tbDriverStandings');
+
+  position = getPosition(data);
+  // loop through array and exclude positions for teams with data.length === 10
+  if (data.length === 20) {
+    position.forEach(function (number) {
+      //create row and give it an ID
+      let row = document.createElement('tr');
+      row.id = 'entry';
+      // create cells, fill them with values from array and append to row
+      let cellPos = document.createElement('td');
+      cellPos.innerHTML = number;
+      row.appendChild(cellPos);
+      // append row to table body
+      bodyDriverStandings.appendChild(row);
+    });
+  }
+  // append table body to table
+  tableDriverStandings.appendChild(bodyDriverStandings);
+}
+
+// TEAM RANKINKS
 let endPointTeams = `rankings/teams?season=2021`;
 callAPI(endPointTeams);
 
+// SCHEDULE OVERVIEW
 const time = Date.now();
 const today = new Date(time);
 let dateForCall = today.toISOString().slice(0, 10);
 console.log(dateForCall);
 let endPointCurrRace = `races?season=2021&date=${dateForCall}`;
 callAPI(endPointCurrRace);
-
 let endPointPrevRace = `races?type=race&season=2021&last=1`;
 callAPI(endPointPrevRace);
-
 let endPointNextRace = `races?type=race&season=2021&next=1`;
 callAPI(endPointNextRace);
+
+function displayDriverStanding(data) {
+  addPositions(data);
+}
