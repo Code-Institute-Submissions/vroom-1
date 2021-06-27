21fa4e71a690ead3f1f2915539374039fafd884c
Ryagg <58631455+Ryagg@users.noreply.github.com>
2021-06-27 13:13:08 +0200

    major change: store endpoints in map, iterate map and save data to localStorage

--- assets/js/displayData.js
+++ assets/js/displayData.js
@@ -1,30 +1,50 @@
-async function callAPI(endPoint) {
-  fetch(`https://api-formula-1.p.rapidapi.com/${endPoint}`, {
+// define endpoints for API call and add to Map
+const endpoints = new Map()
+const endPointDR = `rankings/drivers?season=2021`;
+endpoints.set('racerRankings', `${endPointDR}`)
+const endPointTeams = `rankings/teams?season=2021`;
+endpoints.set('constructorRankings', `${endPointTeams}`)
+const time = Date.now();
+const today = new Date(time);
+let dateForCall = today.toISOString().slice(0, 10);
+const endPointCurrRace = `races?season=2021&date=${dateForCall}`;
+endpoints.set('currentRace', `${endPointCurrRace}`);
+const endPointPrevRace = `races?type=race&season=2021&last=1`;
+endpoints.set('prevRace', `${endPointPrevRace}`);
+const endPointNextRace = `races?type=race&season=2021&next=1`;
+endpoints.set('nextRace', `${endPointNextRace}`);
+
+async function callAPI() {
+  // iterate Map with for..of to get data from each endpoint
+  for (let [key, value] of endpoints) {
+    fetch(`https://api-formula-1.p.rapidapi.com/${value}`, {
     method: 'GET',
     headers: {
       'x-rapidapi-key': '0976a2e9aemsh7e7a4e1e87da560p10b7a1jsnf97cc271f7ab',
       'x-rapidapi-host': 'api-formula-1.p.rapidapi.com',
     },
   })
-    .then(async (response) => {
-      let result = await response.json();
-      let data = result.response;
-      console.log(data);
-      displayDriverStanding(data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  .then(async (response) => {
+    let result = await response.json();
+    let data = result.response;
+    // save to localStorage to avoid name conflicts
+    // because several endpoints have  attributes 'name', 'points', 'position' etc.
+    localStorage.setItem(`${key}`, JSON.stringify(data))
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+  }
+
 }
 
-// DRIVER RANKINGS
-let endPointDR = `rankings/drivers?season=2021`;
-callAPI(endPointDR);
+displayDriverStanding()
 
 // retrieve positions from response and store in array
-function getPosition(data) {
+function getDriverPosition() {
   let position = [];
-  data.forEach(function (element) {
+  let racerRankings = JSON.parse(localStorage.getItem('racerRankings'))
+  racerRankings.forEach(function (element) {
     let pos = element.position;
     position.push(pos);
   });
@@ -32,15 +52,13 @@ function getPosition(data) {
 }
 
 // add Driverpositions to table
-function addPositions(data) {
+function addPositions() {
   const tableDriverStandings = document.getElementById('driverStandings');
   const bodyDriverStandings = document.getElementById('tbDriverStandings');
 
-  position = getPosition(data);
-  // loop through array and exclude positions for teams with data.length === 10
-  if (data.length === 20) {
+  position = getDriverPosition();
     position.forEach(function (number) {
-      //create row and give it an ID
+    //create row and give it an ID
       let row = document.createElement('tr');
       row.id = 'entry';
       // create cells, fill them with values from array and append to row
@@ -50,27 +68,18 @@ function addPositions(data) {
       // append row to table body
       bodyDriverStandings.appendChild(row);
     });
-  }
+
   // append table body to table
   tableDriverStandings.appendChild(bodyDriverStandings);
 }
 
-// TEAM RANKINKS
-let endPointTeams = `rankings/teams?season=2021`;
-callAPI(endPointTeams);
+console.log(endpoints);
+console.log(JSON.parse(localStorage.getItem('racerRankings')));
+console.log(JSON.parse(localStorage.getItem('constructorRankings')));
+console.log(JSON.parse(localStorage.getItem('nextRace')));
+console.log(JSON.parse(localStorage.getItem('currentRace')));
+console.log(JSON.parse(localStorage.getItem('prevRace')));
 
-// SCHEDULE OVERVIEW
-const time = Date.now();
-const today = new Date(time);
-let dateForCall = today.toISOString().slice(0, 10);
-console.log(dateForCall);
-let endPointCurrRace = `races?season=2021&date=${dateForCall}`;
-callAPI(endPointCurrRace);
-let endPointPrevRace = `races?type=race&season=2021&last=1`;
-callAPI(endPointPrevRace);
-let endPointNextRace = `races?type=race&season=2021&next=1`;
-callAPI(endPointNextRace);
-
-function displayDriverStanding(data) {
-  addPositions(data);
+function displayDriverStanding() {
+  addPositions()
 }
